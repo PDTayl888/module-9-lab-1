8d40b996cbc446b881ef3a52fe50f4b6cb53dc08
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-23 21:08:59 -0500

    styling

--- component-library/src/App.tsx
+++ component-library/src/App.tsx
@@ -1,56 +1,73 @@
-import { useState } from 'react';
-import './App.css'
-import AlertBox from './components/AlertBox/AlertBox'
-import ProductDisplay from './components/ProductDisplay/ProductDisplay.tsx';
-import UserProfileCard from './components/UserProfileCard/UserProfileCard'
-import type {User} from './types/index.ts'
+import { useState } from "react";
+import "./App.css";
+import AlertBox from "./components/AlertBox/AlertBox";
+import ProductDisplay from "./components/ProductDisplay/ProductDisplay.tsx";
+import UserProfileCard from "./components/UserProfileCard/UserProfileCard";
+import type { User } from "./types/index.ts";
 
 function App() {
-
   const [showAlert, setShowAlert] = useState(false);
   const [cartItems, setCartItems] = useState<string[]>([]);
 
   const product = {
-  id: '1',
-  name: 'Wireless Headphones',
-  price: 199.99,
-  description: 'High-quality wireless headphones with noise cancellation.',
-  imageUrl: 'https://example.com/headphones.jpg',
-  inStock: true
-};
+    id: "1",
+    name: "Wireless Headphones",
+    price: 199.99,
+    description: "High-quality wireless headphones with noise cancellation.",
+    imageUrl: "https://example.com/headphones.jpg",
+    inStock: true,
+  };
 
-   const tempUser:User = {
-        id:"1",
-        name: "Bob",
-        email: "bobby@gmail.com",
-        role: "admin",
-        avatarUrl: "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F012%2F749%2F491%2Foriginal%2Fsurprised-pikachu-meme-icon-free-vector.jpg&sp=1770664084T95010a48497b490cd4473cd8683ed2907ba24507b48faff2ace6e148f5f0cbd6"
-    };
+  const tempUser: User = {
+    id: "1",
+    name: "Bob",
+    email: "bobby@gmail.com",
+    role: "admin",
+    avatarUrl:
+      "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F012%2F749%2F491%2Foriginal%2Fsurprised-pikachu-meme-icon-free-vector.jpg&sp=1770664084T95010a48497b490cd4473cd8683ed2907ba24507b48faff2ace6e148f5f0cbd6",
+  };
 
-     const handleAddToCart = (productId: string) => {
+  const handleAddToCart = (productId: string) => {
+    console.log("HANDLE ADD TO CART CALLED");
     setCartItems([...cartItems, productId]);
     setShowAlert(true);
   };
- 
 
   return (
     <>
-    {showAlert && (
-      <AlertBox type='success' message='Hello World' onClose={() => setShowAlert(false)}>
-        Testing
-      </AlertBox>)}
-
-      <UserProfileCard user= {tempUser} showEmail={true}>
+      <UserProfileCard user={tempUser} showEmail={true} showRole={true}>
+        <h1>MEGA MART</h1>
       </UserProfileCard>
 
       <ProductDisplay
         product={product}
         showDescription={true}
-        showStockStatus={true} 
-        onAddToCart={handleAddToCart}   
+        showStockStatus={true}
+        onAddToCart={handleAddToCart}
       ></ProductDisplay>
+
+      <h2>Test section 2</h2>
+
+      <ProductDisplay
+        product={{ ...product, inStock: false }}
+        showDescription={true}
+        showStockStatus={false}
+        onAddToCart={handleAddToCart}
+      >
+        <p style={{ color: "green" }}>AVAILABLE APRIL 7</p>
+      </ProductDisplay>
+
+      {showAlert && (
+        <AlertBox
+          type="success"
+          message="ITEM ADDED"
+          onClose={() => setShowAlert(false)}
+        >
+          ADD COUPONS IN CART
+        </AlertBox>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- component-library/src/components/UserProfileCard/UserProfileCard.tsx
+++ component-library/src/components/UserProfileCard/UserProfileCard.tsx
@@ -1,16 +1,17 @@
 import type { UserProfileCardProps } from '../../types'
 
-function UserProfileCard({user, showEmail,showRole, onEdit,children}: UserProfileCardProps) {
+function UserProfileCard({user, showEmail, showRole, onEdit,children}: UserProfileCardProps) {
     return (
-        <>
-        <h1>Title</h1>
+        <div style={{border: '1px solid #fffffff',
+            borderRadius: '8px', margin: '29px', backgroundColor: '#70a385',}}>
+        <h4>USER PROFILE: </h4>
         <p>{user.name}</p>
         {showEmail && <p>Email: {user.email}</p>}
         {showRole && <p>Role: {user.role}</p>}
         {onEdit && 
-            (<button onClick={() => onEdit(user.id)}>Edit Profile</button>)};
+            (<button onClick={() => onEdit(user.id)}>Edit Profile</button>)}
         {children}
-        </>
+        </div>
     )
 }
 
